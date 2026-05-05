bf2ea26afbdc81e536fc5a6b34f7e3034d03ca06
GIAN GABRIEL DOS SANTOS GALLIZIOLI <gian.gsgallizioli@senacsp.edu.br>
2026-05-05 16:24:29 -0300

    estilizando pedido

--- src/views/ListaPedidosView.vue
+++ src/views/ListaPedidosView.vue
@@ -8,7 +8,7 @@ import { useAuth } from '@/composables/useAuth'
 const pedidos = ref<PedidoModel[]>([]);
 const email = localStorage.getItem("user_email") || "";
 
-const { estaLogado }= useAuth();
+const { estaLogado } = useAuth();
 
 
 const carregarPedidos = async () => {
@@ -27,62 +27,74 @@ onMounted(() => {
 });
 </script>
 <template>
-  <div class="container mt-5 vh-100 " v-if="estaLogado">
-    <h1 class="mb-4">Meus Pedidos</h1>
+  <div class="background">
+    <div class="container mt-5 vh-100 " v-if="estaLogado">
+      <h1 class="mb-4">Meus Pedidos</h1>
 
-    <div v-if="pedidos.length === 0" class="alert alert-info">
-      Você ainda não realizou nenhum pedido.
-    </div>
-
-    <div v-for="(pedido) in pedidos" :key="pedido.id" class="card mb-4 shadow-sm">
-      <div class="card-header bg-dark text-white d-flex justify-content-between">
-        <span
-          ><strong>Pedido #{{ pedido.id }}</strong></span
-        >
-        <span>Data: {{ new Date(pedido.dataPedido).toLocaleDateString() }}</span>
+      <div v-if="pedidos.length === 0" class="alert alert-info">
+        Você ainda não realizou nenhum pedido.
       </div>
 
-      <div class="card-body">
-        <table class="table table-hover">
-          <thead>
-            <tr>
-              <th>Produto</th>
-              <th>Qtd</th>
-              <th>Valor Unit.</th>
-              <th>Subtotal</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in pedido.itens" :key="item.id">
-              <td>{{ item.nome }}</td>
-              <td>{{ item.quantidade }}</td>
-              <td>{{ formatarMoeda(item.valor) }}</td>
-              <td>{{ formatarMoeda(item.valorTotal) }}</td>
-            </tr>
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colspan="3" class="text-end"><strong>Total do Pedido:</strong></td>
-              <td class="text-success">
-                <strong>{{ formatarMoeda(pedido.valorTotalPedido) }}</strong>
-              </td>
-            </tr>
-          </tfoot>
-        </table>
+      <div v-for="(pedido) in pedidos" :key="pedido.id" class="card mb-4 shadow-sm">
+        <div class="card-header bg-dark text-white d-flex justify-content-between">
+          <span><strong>Pedido #{{ pedido.id }}</strong></span>
+          <span>Data: {{ new Date(pedido.dataPedido).toLocaleDateString() }}</span>
+        </div>
 
-        <div class="mt-2 small text-muted">
-          <strong>Entregar em:</strong> {{ pedido.endereco }}, {{ pedido.cidade }} - CEP:
-          {{ pedido.cep }}
-        </div><br>
-        <div>
-          <RouterLink class="btn btn-success" to="/pagamento">Ir para pagamento</RouterLink>
+        <div class="card-body">
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th>Produto</th>
+                <th>Qtd</th>
+                <th>Valor Unit.</th>
+                <th>Subtotal</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in pedido.itens" :key="item.id">
+                <td>{{ item.nome }}</td>
+                <td>{{ item.quantidade }}</td>
+                <td>{{ formatarMoeda(item.valor) }}</td>
+                <td>{{ formatarMoeda(item.valorTotal) }}</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colspan="3" class="text-end"><strong>Total do Pedido:</strong></td>
+                <td class="text-success">
+                  <strong>{{ formatarMoeda(pedido.valorTotalPedido) }}</strong>
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+
+          <div class="mt-2 small text-muted">
+            <strong>Entregar em:</strong> {{ pedido.endereco }}, {{ pedido.cidade }} - CEP:
+            {{ pedido.cep }}
+          </div><br>
+          <div>
+            <RouterLink class="btn btn-success" to="/pagamento">Ir para pagamento</RouterLink>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  <div class="container mt-5 vh-100" v-else>
-    <h1>Faça o login para exibir seus pedidos</h1>
-    <RouterLink class="btn btn-primary" to="/login">Ir para o Login...</RouterLink>
+    <div class="container mt-5 vh-100" v-else>
+      <h1>Faça o login para exibir seus pedidos</h1>
+      <RouterLink class="btn btn-primary" to="/login">Ir para o Login...</RouterLink>
+    </div>
   </div>
 </template>
-<style scoped></style>
+<style scoped>
+.background,html {
+    background: radial-gradient(circle at top, #1a2a33 0%, #111 100%);
+}
+
+h1 {
+  color: white;
+}
+
+.card-header{
+background-color: #3B8CBE !important;
+}
+</style>
